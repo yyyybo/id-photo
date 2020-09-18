363c9a8d6e2ea6a2413eaead871fda2a061dc189
yibo.yu <worktobo@163.com>
2020-09-18 11:52:46 +0800

    证件照制作

---
+++ src/main/java/com/xb/photo/IdPhotoApplication.java
@@ -0,0 +1,18 @@
+package com.xb.photo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 入口: 证件照制作
+ *
+ * @author 莫问
+ * @date 2020-09-18
+ */
+@SpringBootApplication
+public class IdPhotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdPhotoApplication.class, args);
+    }
+}
